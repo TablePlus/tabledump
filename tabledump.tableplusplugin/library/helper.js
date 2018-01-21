0fedd65006ff9ced0fa85ae08218bcef59611559
Huy Pham <duchuykun@gmail.com>
2018-01-21 15:11:23 +0700

    Refactor code

--- tabledump.tableplusplugin/library/helper.js
+++ tabledump.tableplusplugin/library/helper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Example create function
-var getCreationQuery = function(item) {
+function getCreationQuery(item) {
 	return 'SHOW CREATE ' + item.type().toUpperCase() + ' ' + item.name() + ';'
 }
 
@@ -9,4 +9,23 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-export { getCreationQuery, capitalizeFirstLetter};
+function handleDumpMySQL(context, item) {
+	var creationQuery = getCreationQuery(item);
+    var type = capitalizeFirstLetter(item.type());
+
+    context.execute(creationQuery, (result) => {
+        if (result.rows.length == 0) {return}
+        var row = result.rows[0]
+        var creation = row.raw('Create ' + type);
+        SystemService.runInMain(function() {
+            SystemService.insertToClipboard(creation);           
+            SystemService.notify('Copy creation', type + ' ' + item.name() + ' creation is copied!'); 
+        });
+    });
+}
+
+function handleDumpPostgreSQL(context, item) {
+    context.alert('Warning', 'Being developed');
+}
+
+export { handleDumpMySQL, handleDumpPostgreSQL };
--- tabledump.tableplusplugin/main.js
+++ tabledump.tableplusplugin/main.js
@@ -1,31 +1,26 @@
 'use strict';
 
-import { getCreationQuery, capitalizeFirstLetter } from './library/helper';
+import { handleDumpMySQL, handleDumpPostgreSQL } from './library/helper';
 
 var onRun = function(context) {
-    // Check support
-    if (context.driver() != 'MySQL') {
-        context.alert('WIP', 'Only support MySQL for now, check back later');
-        return;
-    }
-
     // Get table in opening tab
-    var item = context.clickedItem();
+    let item = context.clickedItem();
     if (item == null) {
         context.alert('Error', 'Please select a Table');
         return;
     }
-    var creationQuery = getCreationQuery(item);
-    var type = capitalizeFirstLetter(item.type());
-    context.execute(creationQuery, (result) => {
-        if (result.rows.length == 0) {return}
-        var row = result.rows[0]
-        var creation = row.raw('Create ' + type);
-        SystemService.runInMain(function() {
-            SystemService.insertToClipboard(creation);           
-            SystemService.notify('Copy creation', type + ' ' + item.name() + ' creation is copied!'); 
-        });
-    });
+
+    let driver = context.driver();
+    switch(driver) {
+    case 'MySQL':
+        handleDumpMySQL(context, item);
+        break;
+    case 'PostgreSQL':
+        handleDumpPostgreSQL(context, item);
+        break;
+    default:
+        context.alert('Warning', 'Being developed');
+    } 
 };
 
 global.onRun = onRun;
