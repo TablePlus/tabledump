a6720d0c2e20f224de6397f0d10b86b9f062b087
Huy Pham <duchuykun@gmail.com>
2018-05-02 12:25:24 +0700

    Update helper.js

--- tabledump.tableplusplugin/library/helper.js
+++ tabledump.tableplusplugin/library/helper.js
@@ -3,7 +3,6 @@
 // Example create function
 function handleDumpTableDefinition(context, item) {
 	context.itemDefinition(item, function(creation) {
-        // Get table columns listing
         SystemService.runInMain(function() {
             SystemService.insertToClipboard(creation);
             SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' creation is copied!');
