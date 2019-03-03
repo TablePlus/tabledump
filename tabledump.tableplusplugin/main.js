591f78847af7af9aa475b552ff10a828a9a09070
Huy Pham <duchuykun@gmail.com>
2019-03-03 21:42:56 +0700

    Added laravel export.

--- tabledump.tableplusplugin/main.js
+++ tabledump.tableplusplugin/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { handleDumpTableDefinition } from './library/helper';
+import { dumpTableAsDefinition, dumpTableAsLaravel } from './library/helper';
 
 var creation = function(context) {
     // Get table in opening tab
@@ -9,7 +9,7 @@ var creation = function(context) {
         context.alert('Error', 'Please select a Table');
         return;
     }
-    handleDumpTableDefinition(context, item);
+    dumpTableAsDefinition(context, item);
 };
 
 var drop = function(context) {
@@ -19,10 +19,8 @@ var drop = function(context) {
         context.alert('Error', 'Please select a Table');
         return;
     }
-    SystemService.runInMain(function() {
-        SystemService.insertToClipboard('DROP ' + item.type() + ' ' + item.nameWithQuotes() + ';');
-        SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' drop statement is copied!');
-    });
+    SystemService.insertToClipboard('DROP ' + item.type() + ' ' + item.nameWithQuotes() + ';');
+    SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' drop statement is copied!');
 };
 
 var truncate = function(context) {
@@ -32,12 +30,21 @@ var truncate = function(context) {
         context.alert('Error', 'Please select a Table');
         return;
     }
-    SystemService.runInMain(function() {
-        SystemService.insertToClipboard('TRUNCATE ' + item.type() +  ' ' + item.nameWithQuotes() + ';');
-        SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' truncate statement is copied!');
-    });
+    SystemService.insertToClipboard('TRUNCATE ' + item.type() +  ' ' + item.nameWithQuotes() + ';');
+    SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' truncate statement is copied!');
 };
+
+var laravel = function(context)  {
+    // Get table in opening tab
+    let item = context.clickedItem();
+    if (item == null) {
+        context.alert('Error', 'Please select a Table');
+        return;
+    }
+    dumpTableAsLaravel(context, item);
+}
 
 global.creation = creation;
 global.drop = drop;
 global.truncate = truncate;
+global.laravel = laravel;
