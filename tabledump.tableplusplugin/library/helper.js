0ec39db81b9a82e5d6897e414291efdef78f9779
Huy Pham <duchuykun@gmail.com>
2019-03-04 10:33:11 +0700

    Support binary, chars, bigint.

--- tabledump.tableplusplugin/library/helper.js
+++ tabledump.tableplusplugin/library/helper.js
@@ -1,67 +1,85 @@
-'use strict';
+"use strict";
 
 function dumpTableAsDefinition(context, item) {
-	context.itemDefinition(item, function(creation) {
-      SystemService.insertToClipboard(creation);
-      SystemService.notify('Copy creation', item.type() + ' ' + item.name() + ' creation statement is copied!');
+  context.itemDefinition(item, function(creation) {
+    SystemService.insertToClipboard(creation);
+    SystemService.notify(
+      "Copy creation",
+      item.type() + " " + item.name() + " creation statement is copied!"
+    );
   });
 }
 
 function camelize(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-    return letter.toUpperCase();
-  }).replace(/\s+|-|_/g, '');
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+      return letter.toUpperCase();
+    })
+    .replace(/\s+|-|_/g, "");
 }
 
 function getColumnMigrate(columnName, dataType, isNullable) {
-   var typeArr = dataType.split("(");
-   var typeOnly = typeArr[0];
-   var typeLength = "";
-   if (typeArr.length > 1) {
-       typeLength = typeArr[1];
-   }
-   var migration = "";
-   switch(typeOnly) {
-	  case "varchar":
-        if (typeLength.length > 0) {
-            migration = "$table->string('" + columnName + "', " + typeLength + "";
-        } else {
-            migration = "$table->string('" + columnName + "')";          
-        }
-        break;
-	  case "text":
-	      migration = "$table->string('" + columnName + "')";
-        break;
+  var typeArr = dataType.split("(");
+  var typeOnly = typeArr[0];
+  var typeLength = "";
+  if (typeArr.length > 1) {
+    typeLength = typeArr[1];
+  }
+  var migration = "";
+  switch (typeOnly) {
+    case "varchar":
+      if (typeLength.length > 0) {
+        migration = "$table->string('" + columnName + "', " + typeLength + "";
+      } else {
+        migration = "$table->string('" + columnName + "')";
+      }
+      break;
+    case "text":
+      migration = "$table->string('" + columnName + "')";
+      break;
+    case "char":
+      migration = "$table->char('" + columnName + "', " + typeLength + "";
+      break;
+    case "binary":
+      migration = "$table->binary('" + columnName + "')";
+      break;
+    case "bigint":
+      if (dataType.includes("unsigned")) {
+        migration = "$table->unsignedBigInteger('" + columnName + "')";
+      } else {
+        migration = "$table->bigInteger('" + columnName + "')";
+      }
+      break;
     case "int":
     case "int4":
-          if (dataType.includes("unsigned")) {
-              migration = "$table->bigIncrements('" + columnName + "')"
-          } else {
-              migration = "$table->integer('" + columnName + "')"            
-          }
-          break;
+      if (dataType.includes("unsigned")) {
+        migration = "$table->bigIncrements('" + columnName + "')";
+      } else {
+        migration = "$table->integer('" + columnName + "')";
+      }
+      break;
     case "tinyint":
-          if (dataType.includes("unsigned")) {
-              migration = "$table->unsignedTinyInteger('" + columnName + "')"
-          } else {
-              migration = "$table->tinyInteger('" + columnName + "')"            
-          }
-          break;
-	  default:
-	      migration = "$table->unsupported('" + columnName + "')";
-        break;
-	}
-  if (isNullable.toLowerCase().charAt(0) == 'y') {
-      migration += "->nullable()";
+      if (dataType.includes("unsigned")) {
+        migration = "$table->unsignedTinyInteger('" + columnName + "')";
+      } else {
+        migration = "$table->tinyInteger('" + columnName + "')";
+      }
+      break;
+    default:
+      migration = "$table->unsupported('" + columnName + "')";
+      break;
+  }
+  if (isNullable.toLowerCase().startsWith("y")) {
+    migration += "->nullable()";
   }
   return migration + ";";
 }
 
 function dumpTableAsLaravel(context, item) {
-// Currently only work with MySQL
+  // Currently only work with MySQL
 
   var nameCamelcase = camelize(item.name());
-	var header = `<?php
+  var header = `<?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -83,42 +101,46 @@ class Create${nameCamelcase}Table extends Migration
     {
         Schema::create('${item.name()}', function (Blueprint $table) {
 `;
-	var columnNames = [];
-	var columnTypes = [];
+  var columnNames = [];
+  var columnTypes = [];
   var isNullables = [];
   var defaultVals = [];
   var query;
   var driver = context.driver();
   switch (driver) {
-      case 'MySQL':
-          query = `SELECT ordinal_position as ordinal_position,column_name as column_name,column_type AS data_type,is_nullable as is_nullable,column_default as column_default,extra as extra,column_name AS foreign_key,column_comment AS comment FROM information_schema.columns WHERE table_schema='${item.schema()}'AND table_name='${item.name()}';`
-          break;
-      case 'PostgreSQL':
-          query = `SELECT ordinal_position,column_name,udt_name AS data_type,numeric_precision,datetime_precision,numeric_scale,character_maximum_length AS data_length,is_nullable,column_name as check,column_name as check_constraint,column_default,column_name AS foreign_key,pg_catalog.col_description(16402,ordinal_position) as comment FROM information_schema.columns WHERE table_name='${item.name()}'AND table_schema='${item.schema()}';`
-          break;
-      default:
-          context.alert('Rrror', driver + ' is not supported');
-          return;
+    case "MySQL":
+      query = `SELECT ordinal_position as ordinal_position,column_name as column_name,column_type AS data_type,is_nullable as is_nullable,column_default as column_default,extra as extra,column_name AS foreign_key,column_comment AS comment FROM information_schema.columns WHERE table_schema='${item.schema()}'AND table_name='${item.name()}';`;
+      break;
+    case "PostgreSQL":
+      query = `SELECT ordinal_position,column_name,udt_name AS data_type,numeric_precision,datetime_precision,numeric_scale,character_maximum_length AS data_length,is_nullable,column_name as check,column_name as check_constraint,column_default,column_name AS foreign_key,pg_catalog.col_description(16402,ordinal_position) as comment FROM information_schema.columns WHERE table_name='${item.name()}'AND table_schema='${item.schema()}';`;
+      break;
+    default:
+      context.alert("Rrror", driver + " is not supported");
+      return;
   }
-	context.execute(query, (res) => {
-	    res.rows.forEach((row) => {
-	        let columnName = row.raw('column_name');
-	        let columnType = row.raw('data_type');
-          let isNullable = row.raw('is_nullable');
-          let defaultVal = row.raw('column_default');
-	        columnNames.push(columnName);
-	        columnTypes.push(columnType);
-          isNullables.push(isNullable);
-          defaultVals.push(defaultVal);
-	    });
-	    var result = header;
-	    for (let i = 0; i < columnNames.length; i++) { 
-	    	var columnMigrate = getColumnMigrate(columnNames[i], columnTypes[i], isNullables[i]);
-	    	result += `            ${columnMigrate}\n`;
-	    };
-	    result += `        });
+  context.execute(query, res => {
+    res.rows.forEach(row => {
+      let columnName = row.raw("column_name");
+      let columnType = row.raw("data_type");
+      let isNullable = row.raw("is_nullable");
+      let defaultVal = row.raw("column_default");
+      columnNames.push(columnName);
+      columnTypes.push(columnType);
+      isNullables.push(isNullable);
+      defaultVals.push(defaultVal);
+    });
+    var result = header;
+    for (let i = 0; i < columnNames.length; i++) {
+      var columnMigrate = getColumnMigrate(
+        columnNames[i],
+        columnTypes[i],
+        isNullables[i]
+      );
+      result += `            ${columnMigrate}\n`;
     }
-	 
+    result += `        });
+    }
+   
     /**
      * Reverse the migrations.
      *
@@ -130,9 +152,12 @@ class Create${nameCamelcase}Table extends Migration
     }
 }
 `;
-      SystemService.insertToClipboard(result);
-      SystemService.notify('Laravel export', item.type() + ' ' + item.name() + ' export statement is copied!');
-	});
+    SystemService.insertToClipboard(result);
+    SystemService.notify(
+      "Laravel export",
+      item.type() + " " + item.name() + " export statement is copied!"
+    );
+  });
 }
 
 export { dumpTableAsDefinition, dumpTableAsLaravel };
