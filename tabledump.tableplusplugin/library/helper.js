60c418996fc7436dde5468cc9ab89214a0885d5f
decaylala <decaylala@users.noreply.github.com>
2019-12-11 19:08:10 +0800

    Improve class name of snake_case type table name

--- tabledump.tableplusplugin/library/helper.js
+++ tabledump.tableplusplugin/library/helper.js
@@ -12,7 +12,7 @@ function dumpTableAsDefinition(context, item) {
 
 function camelize(str) {
   return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    .replace(/(?:^\w|[A-Z]|\b\w|_\w)/g, function(letter, index) {
       return letter.toUpperCase();
     })
     .replace(/\s+|-|_/g, "");
